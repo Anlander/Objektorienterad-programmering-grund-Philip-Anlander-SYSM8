0e1e7ca4e55547857574a2bfad65704eade5a6e0
anlander <anlanderp@gmail.com>
2024-10-22 14:00:03 +0200

    modify gitignore

--- Fit_Tracker/obj/Debug/net8.0-windows/Fit_Tracker.AssemblyInfo.cs
+++ Fit_Tracker/obj/Debug/net8.0-windows/Fit_Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fit_Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f63fcc946b85a8fb69e84230be46646b1fbbbe5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc7556341a823edba6a41ef610532135e31dba81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fit_Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fit_Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
